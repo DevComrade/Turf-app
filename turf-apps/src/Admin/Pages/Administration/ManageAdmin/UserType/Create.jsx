31a17a659b3d0144745bfc271834c71efa067836
kathirviswa <kathirviswa57@gmail.com>
2024-12-16 19:06:29 +0530

    b
    b

--- turf-apps/src/Admin/Pages/Administration/ManageAdmin/UserType/Create.jsx
+++ turf-apps/src/Admin/Pages/Administration/ManageAdmin/UserType/Create.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 
 const CreateForm = () => {
   const [formData, setFormData] = useState({
--- turf-apps/src/Admin/Pages/Administration/ManageAdmin/UserType/Edit.jsx
+++ turf-apps/src/Admin/Pages/Administration/ManageAdmin/UserType/Edit.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 
 const EditForm = () => {
   const [formData, setFormData] = useState({
